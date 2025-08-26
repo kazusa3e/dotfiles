c79f561d612dcaad72d51b46c710b62ab4ee50f0
kazusa <i@kazusa.me>
2025-08-26 22:25:39 +0800

    fix(hammerspoon): change window margin to 10% when let window center

--- hammerspoon/.hammerspoon/win-sizing.lua
+++ hammerspoon/.hammerspoon/win-sizing.lua
@@ -25,8 +25,10 @@ hs.urlevent.bind('win-sizing', function (eventName, params)
     elseif (params.method == 'max') then
         f.x = max.x f.y = max.y f.w = max.w f.h = max.h
     elseif (params.method == 'center') then
-        -- margin: 15%
-        f.x = max.x + (max.w * 15 / 100) f.y = max.y + (max.h * 15 / 100) f.w = max.w * 70 / 100 f.h =max.h * 70 / 100
+        -- margin: 10%
+        f.x = max.x + (max.w * 10 / 100) f.y = max.y + (max.h * 10 / 100) f.w = max.w * 80 / 100 f.h =max.h * 80 / 100
+        -- -- margin: 15%
+        -- f.x = max.x + (max.w * 15 / 100) f.y = max.y + (max.h * 15 / 100) f.w = max.w * 70 / 100 f.h =max.h * 70 / 100
         -- f.x = max.x + (max.w * 20 / 100) f.y = max.y + (max.h * 20 / 100) f.w = max.w * 60 / 100 f.h =max.h * 60 / 100
     elseif (params.method == 'r') then
         f.x = max.x f.y = max.y f.w = max.w / 3 * 2 f.h = max.h
